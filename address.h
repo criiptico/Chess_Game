6e85e42de23d7c47750fb5dc923bf090050d4ad2
Cryptic_Rogue1 <53357237+criiptico@users.noreply.github.com>
2022-06-13 10:47:37 -0500

    I just wanted to take one last crack at it by adding lines 9 and 21. I thought I could make a template function but it look like it worked. I could consider- I'm not sure

--- address.h
+++ address.h
@@ -4,9 +4,10 @@
 template <typename T>
 class address{
 public:
-    address(T{a}, list<int>{0});
-    enum rank{a, b, c, d, e, f, g, h};
-    list<int> file{1, 2, 3, 4, 5, 6, 7, 8};
+    address(T&, list<int>);
+    enum rank{a{1}, b{2} c{3}, d{4}, e{5}, f{6}, g{7}, h{8}};
+    enum file{one{1}, two{2}, three{3}, four{4}, five{5}, six{6}, seven{7}, eight{8}};
+  //  list<int> file{1, 2, 3, 4, 5, 6, 7, 8};
     
 
     
@@ -17,7 +18,8 @@ private:
     
 };
 
-//address(T horizontal, list<int> vertical{0} ):{ rank = horizontal; file = vertical;}
+template <typename T>
+address(T& horizontal, T& vertical ){ rank = horizontal; file = vertical;}
 
 
 #endif
