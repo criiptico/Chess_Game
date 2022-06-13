206c4c6bc86760888112d474bd0f9e949bae11d9
Cryptic_Rogue1 <53357237+criiptico@users.noreply.github.com>
2022-06-13 13:30:54 -0500

    Update address.h
    I know I said I'd look at something else, but I want to try solving this problem. The concept of having rank and file as enumerators is great, but I can't figure out how to use them in a constructor. I could always make a setRank and a setFile too. Maybe it'd be easier. But the problem wasn't that they're both together- it's that I can't seem t- what if I tried it without a template?
    
    I also tried to do it in a list, but I think I'm missing syntax or something. I'm not so sure what the problem is.

--- address.h
+++ address.h
@@ -1,25 +1,39 @@
 #ifndef ADDRESS_H
 #define ADDRESS_H
 
-template <typename T>
+#include <iostream>
+//template <typename T>
 class address{
 public:
-    address(T&, T&);
-    enum rank{a{1}, b{2} c{3}, d{4}, e{5}, f{6}, g{7}, h{8}};
-    enum file{one{1}, two{2}, three{3}, four{4}, five{5}, six{6}, seven{7}, eight{8}};
-  //  list<int> file{1, 2, 3, 4, 5, 6, 7, 8};
-    
+    //template<typename T> address(T&, T&) -> address<T>;
+    // address(T&, T&);
+    // enum rank{a{1}, b{2} c{3}, d{4}, e{5}, f{6}, g{7}, h{8}};
+    // enum file{one{1}, two{2}, three{3}, four{4}, five{5}, six{6}, seven{7}, eight{8}};
+//
 
-    
+//The bottom might be easier to work with. But I like the idea of enumeration.
+    list<char> rank {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    list<int> file{1, 2, 3, 4, 5, 6, 7, 8};
+    address(char, int);
+
 
 private:
-    rank horizontal{a};
-    int vertical{1};
-    
+    char rank {'a'};
+    int file{1};
+   // template<typename T> address(T& horizontal, T& vertical) -> address<typename T>;
 };
 
-template <typename T>
-address(T& horizontal, T& vertical ){ rank = horizontal; file = vertical;}
+  address(char horizontal{}, int vertical){
+      rank = horizontal;
+      file = vertical;
+  }
+
+
+// template <typename T>
+// address(T& horizontal, T& vertical ){ 
+//   rank = horizontal; 
+//   file = vertical;
+//   }
 
 
 #endif
