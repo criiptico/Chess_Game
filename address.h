0076d0bbfa0691414786da7be8651dce58f195f9
Cryptic_Rogue1 <53357237+criiptico@users.noreply.github.com>
2022-06-13 12:18:06 -0500

    another change

--- address.h
+++ address.h
@@ -4,7 +4,7 @@
 template <typename T>
 class address{
 public:
-    address(T&, list<int>);
+    address(T&, T&);
     enum rank{a{1}, b{2} c{3}, d{4}, e{5}, f{6}, g{7}, h{8}};
     enum file{one{1}, two{2}, three{3}, four{4}, five{5}, six{6}, seven{7}, eight{8}};
   //  list<int> file{1, 2, 3, 4, 5, 6, 7, 8};
