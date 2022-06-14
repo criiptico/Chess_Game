7aeabfad4e62fdb33fb2cad2481339553b0080a3
Cryptic_Rogue1 <53357237+criiptico@users.noreply.github.com>
2022-06-14 15:31:47 -0500

    Templatizing, Enumeration, Objects, and Constructors.
    The problem was with templatized classes and functions. I now realize I used them with the intention of a data types, when templates are usually used with classes and their objects. In the previous original, address.h, line 5 was meant to use templates for other data types of classes. What I didn't think of was that those data types aren't... classes. The rank and file enumerations are their own data types but not objects. Lines 13-15 were experimental. Lines 17-22 were going to be the alternative if I couldn't solve my initial problem with templates and enumeration. Lines 30-42 is what I tried to do initially.
    The stack overflow post that helped me with this was putting almost exactly what I was doing, in the most simplest terms. But it was essentially the same concept. Here is the link: https://stackoverflow.com/questions/34822378/can-i-use-an-enum-in-a-constructor-for-a-class

--- address.cpp
+++ address.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 #include "address.h"
 
-//   address::address(char horizontal, int vertical){
-//       rank = horizontal;
-//       file = vertical;
-//   }
-
-// template <typename T>
 address::address(address::rank rank, address::file file)
 {
   horizontal = rank;
--- address.h
+++ address.h
@@ -2,42 +2,18 @@
 #define ADDRESS_H
 
 #include <iostream>
-//template <typename T>
 class address{
 public:
-    //template<typename T> address(T&, T&) -> address<T>;
+    
     
     enum rank{a, b, c, d, e, f, g, h};
     enum file{one, two, three, four, five, six, seven, eight};
 
-    // enum rank{a{1}, b{2} c{3}, d{4}, e{5}, f{6}, g{7}, h{8}};
-    // enum file{one{1}, two{2}, three{3}, four{4}, five{5}, six{6}, seven{7}, eight{8}};
-    
     address(rank horizontal, file vertical);
-//
-//The bottom might be easier to work with. But I like the idea of enumeration.
-    // list<char> rank {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
-    // list<int> file{1, 2, 3, 4, 5, 6, 7, 8};
-    //address(char = 'a', int = 1);
-
 
 private:
     rank horizontal;
     file vertical;
-   // template<typename T> address(T& horizontal, T& vertical) -> address<typename T>;
 };
-
-  // address(char horizontal, int vertical){
-  //     rank = horizontal;
-  //     file = vertical;
-  // }
-
-
-// template <typename T>
-// address(T& horizontal, T& vertical ){ 
-//   rank = horizontal; 
-//   file = vertical;
-//   }
-
 
 #endif
