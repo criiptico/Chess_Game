ddcd32aaa9d179c44aa6323aff00a74aa302ee52
Cryptic_Rogue1 <53357237+criiptico@users.noreply.github.com>
2022-07-14 17:31:15 -0500

    kjnkjnkjn

--- chess_board.cpp
+++ chess_board.cpp
@@ -18,18 +18,7 @@ void container_Gen(){
         address::file f{};
         address::piece p{};
         
-<<<<<<< HEAD
-        while(i < 64){
-            
-
-
-
-            r++;
-            i++;
-        }
-=======
         // insert by each rank
->>>>>>> a3d92d77bd1737cd0ec1097fd511cb384f610440
 
 
 
